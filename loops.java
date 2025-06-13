2506d80018e89831742d7c71418ec66fd54d98c7
Naman Middha <127924759+Naman-Middha@users.noreply.github.com>
2025-06-14 04:49:46 +0530

    Add files via upload

---
+++ loops.java
@@ -0,0 +1,45 @@
+import java.util.*;
+// for loop  do while loop and while loop
+public class loops {
+    public static void main(String[] args){
+        // i ++ = i = i +1 ;
+
+        int sum = 0;
+        int counter = 0;
+        for(int i = 1; i < 101; i ++){
+            if(i%2==0) {
+                counter ++ ;
+                System.out.println(i);
+            }
+            sum = sum +i;
+        }
+        System.out.println("total number of even numbers : "+ counter);
+        System.out.println("sum of values from 1 to 100 "+ sum);
+
+        int i = 0;
+        while(i<11){
+            System.out.println(i);
+            i ++;
+        }
+        i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }while (i<11);
+
+        // while checks condition first and do while first perform then checks the condition
+
+        i = 12;
+        while(i<11){
+            System.out.println("i"+"hello");
+            i++;
+            // no output
+        }
+        System.out.println("\n");
+        i = 12;
+        // it will print first and then check condition
+        do {
+            System.out.println(i + " hello");
+        } while(i < 11);// this condition s always false but still it will print for once then check condition and stop
+    }
+}
